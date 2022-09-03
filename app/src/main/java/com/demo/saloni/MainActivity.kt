a50a530e97c74f0e7cb42a464d299d9e85d9581b
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-09-03 16:13:31 +0200

    Initial commit

---
+++ app/src/main/java/com/demo/saloni/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.demo.saloni
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.demo.saloni.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(binding.root)
+    }
+}
