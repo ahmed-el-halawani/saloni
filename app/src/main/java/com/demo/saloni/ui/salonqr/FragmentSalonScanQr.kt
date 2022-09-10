f297a67e5187f12d835e3d9ee554ddf50cf2bdd1
Ebrahim Hussien Alagamy <52678409+ebrahimalagamy@users.noreply.github.com>
2022-09-10 18:46:01 +0200

    edit v2

---
+++ app/src/main/java/com/demo/saloni/ui/salonqr/FragmentSalonScanQr.kt
@@ -0,0 +1,21 @@
+package com.demo.saloni.ui.salonqr
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.demo.saloni.R
+
+class FragmentSalonScanQr : Fragment() {
+    // TODO: Rename and change types of parameters
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_salon_scan_qr, container, false)
+    }
+
+
+}
