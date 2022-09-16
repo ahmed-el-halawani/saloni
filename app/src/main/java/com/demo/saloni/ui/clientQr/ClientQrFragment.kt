541ca3c1662824555ae8f56f11976e8566e7fbe1
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-09-16 12:56:23 +0200

    done client qr functionality

--- app/src/main/java/com/demo/saloni/ui/clientQr/ClientQrFragment.kt
+++ app/src/main/java/com/demo/saloni/ui/clientQr/ClientQrFragment.kt
@@ -1,7 +1,8 @@
 package com.demo.saloni.ui.clientQr
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +10,13 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
-import com.demo.saloni.R
-import com.demo.saloni.data.remote.entities.SalonProfile
 import com.demo.saloni.databinding.FragmentClientQrBinding
-import com.demo.saloni.databinding.FragmentHomeClientBinding
-import com.demo.saloni.databinding.ItemSalonBinding
 import com.demo.saloni.ui.core.BaseFragment
 import com.demo.saloni.ui.core.State
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import com.newcore.easyrecyclergenerator.rvSingleList
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import java.lang.Exception
 
 
 class ClientQrFragment : BaseFragment() {
@@ -36,8 +32,38 @@ class ClientQrFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vm.getReservation().asLiveData().observe(viewLifecycleOwner){
+        binding.ivQr.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        vm.getReservation().asLiveData().observe(viewLifecycleOwner) {
+            hideMainLoading()
+            when (it) {
+                is State.Error -> Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                is State.Loading -> showMainLoading()
+                is State.Success -> binding.ivQr.setImageBitmap(encodeAsBitmap(it.data?.reservationId))
+            }
+        }
+    }
 
+
+    fun encodeAsBitmap(str: String?): Bitmap? {
+        try {
+            val writer = QRCodeWriter()
+            val bitMatrix = writer.encode(str, BarcodeFormat.QR_CODE, 400, 400)
+            val w = bitMatrix.width
+            val h = bitMatrix.height
+            val pixels = IntArray(w * h)
+            for (y in 0 until h) {
+                for (x in 0 until w) {
+                    pixels[y * w + x] = if (bitMatrix[x, y]) Color.BLACK else Color.WHITE
+                }
+            }
+            val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            bitmap.setPixels(pixels, 0, w, 0, 0, w, h)
+            return bitmap
+        }catch (e:Exception){
+            e.printStackTrace()
+            return null;
         }
     }
 
